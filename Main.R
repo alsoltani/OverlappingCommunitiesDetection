e78976149b88b76f75e104acc2a9e08a39e0394d
RobinVogel <robvogel0@gmail.com>
2015-12-24 11:38:32 +0100

    Update of DER algorithm by C. Puppo

--- Main.R
+++ Main.R
@@ -5,14 +5,25 @@
 library(expm)
 library(igraph)
 library(Matrix)
-setwd("/Users/alain/Documents/ENSAE/Statistical\ Analysis\ of\ Network\ Data/Project")
+setwd("C:/Users/Clément/Desktop/Statistical Analysis of Network Data/Projet")
 
 # 1. DER Algorithm.
 # ---------------------
 
-divergence <- function(mu, nu){
-  idx = which(nu != 0 & mu != 0)
-  sum(nu[idx] * log(mu[idx]))
+divergence <- function(nu, mu){
+  idx_mu = which(mu != 0)
+  if(length(idx_mu)==0){
+    sum(nu[idx_mu] * log(mu[idx_mu]))
+  }
+  else{
+    if(sum(nu[-idx_mu]) != 0){
+      -9000
+    }
+    else{
+      sum(nu[idx_mu] * log(mu[idx_mu]))
+    }
+  }
+
 }
 
 der_algorithm <- function(g, L, k){
@@ -43,29 +54,33 @@ der_algorithm <- function(g, L, k){
   # second one the newly formed one.
   
   partition = matrix(0, nrow=2, ncol=n)
-  partition[1, ] = sample(c(rep(seq(k),  n %/% k), seq(n %% k)))
-  partition[2, ] = 0
+  partition[1, ] = 0
+  partition[2, ] = sample(c(rep(seq(k),  n %/% k), seq(n %% k)))
+  
+  #print(c(partition[1, ] != partition[2, ]))
+  
   
   # mu_s : distribution  of the random walk started from pi_s.
   mu = matrix(0, nrow = k, ncol = n)
-  
+  t = 0
   # While the two partition differs
-  while(Reduce("&", partition[1, ] != partition[2, ])){
-    
+  while(Reduce("|", partition[1, ] != partition[2, ])){
+    t = t+1
     # Step 1.
     for(i in 1:k){
-      s = which(partition[1, ] == i)
+      s = which(partition[2, ] == i)
       mu[i,] = apply(w[s, ], 2, function(r) {weighted.mean(r, w=degree(g)[s])})
-    
-    }
-    
+      }
     # Update the old partition.
     partition[1, ] = partition[2, ]
     
     # Step 2a. Construct the divergence matrix.
     divergence_matrix = matrix(0, nrow = n, ncol = k)
     for (i in 1:n){
-      divergence_matrix[i, ] = apply(mu, 1, function(r) {divergence(w[i, ], r)})
+      for(j in 1:k){
+        divergence_matrix[i,j] = divergence(w[i,],mu[j,])
+      }
+      #divergence_matrix[i, ] = apply(mu, 1, function(r) {divergence(w[i, ], r)})
     }
     
     #print(w[n, ])
@@ -75,16 +90,16 @@ der_algorithm <- function(g, L, k){
     for (i in 1:n){
       partition[2, i] = which(divergence_matrix[i, ] == max(divergence_matrix[i, ]))[1]
     }
-    
-    print(c(partition[1, ] != partition[2, ]))
+    print(t)
+    #print(c(partition[1, ] != partition[2, ]))
     
   }
-  
+  print(t)
   list(partition[2, ], mu)
 }
 
 # Read in edges information.
-edgelist = read.csv("./flickrEdges.txt", sep = " ", skip = 3, nrows = 100)[, 1:2]
+edgelist = read.csv("./flickrEdges.txt", sep = " ", skip = 3, nrows = 1000)[, 1:2]
 colnames(edgelist) = c("Node Id", "Node Id")
 
 # Because the vertex IDs in this dataset are numbers, 
@@ -100,5 +115,5 @@ igraph_data = graph.edgelist(edgelist[, 1:2], directed = F)
 igraph_data <- simplify(igraph_data, remove.multiple = TRUE, remove.loops = TRUE)
 
 # DER Algorithm.
-der_algorithm(igraph_data, 5, 3)
+test <- der_algorithm(igraph_data, 5, 3)
 
